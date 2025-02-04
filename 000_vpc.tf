3fc4930f7a45aca6c01c997588961870b1fd7eb2
rearviewmirror <shahbaz.khan@kainos.com>
2025-02-04 06:05:21 +0000

    lb, sg added with natgw fix and terraform fmt

--- 000_vpc.tf
+++ 000_vpc.tf
@@ -2,7 +2,7 @@
 resource "aws_vpc" "shahbaz_vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
-    Name = "shahbaz-vpc-${var.environment}"
+    Name        = "shahbaz-vpc-${var.environment}"
     description = "This is for week 4 exercise vpc-${var.environment}"
   }
 }
--- 040_route_table.tf
+++ 040_route_table.tf
@@ -1,49 +1,62 @@
 # Purpose: Create route tables for public and private subnets and associate them with the respective subnets.
 resource "aws_route_table" "public" {
-    vpc_id = aws_vpc.shahbaz_vpc.id
+  vpc_id = aws_vpc.shahbaz_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.this.id
   }
+  tags = {
+    Name = "shahbaz-public-route-table-${var.environment}"
+  }
 }
 
 resource "aws_route_table" "private" {
-    vpc_id = aws_vpc.shahbaz_vpc.id
+  count  = 2
+  vpc_id = aws_vpc.shahbaz_vpc.id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.this.id
+    nat_gateway_id = aws_nat_gateway.this[count.index].id
+  }
+
+  tags = {
+    Name = "shahbaz-private-route-table-${var.environment}"
   }
 }
 # Public Association
 resource "aws_route_table_association" "public_a" {
-    subnet_id      = aws_subnet.public_subnet_1.id
-    route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public.id
 }
 resource "aws_route_table_association" "public_b" {
-    subnet_id      = aws_subnet.public_subnet_2.id
-    route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public.id
 }
 
 # Private association
 resource "aws_route_table_association" "private_a" {
-    subnet_id      = aws_subnet.private_subnet_1.id
-    route_table_id = aws_route_table.private.id
+  count          = 2
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private[count.index].id
 }
 resource "aws_route_table_association" "private_b" {
-    subnet_id      = aws_subnet.private_subnet_2.id
-    route_table_id = aws_route_table.private.id
+  count          = 2
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private[count.index].id
 }
 resource "aws_route_table_association" "private_c" {
-    subnet_id      = aws_subnet.private_subnet_3.id
-    route_table_id = aws_route_table.private.id
+  count          = 2
+  subnet_id      = aws_subnet.private_subnet_3.id
+  route_table_id = aws_route_table.private[count.index].id
 }
 resource "aws_route_table_association" "private_d" {
-    subnet_id      = aws_subnet.private_subnet_4.id
-    route_table_id = aws_route_table.private.id
+  count          = 2
+  subnet_id      = aws_subnet.private_subnet_4.id
+  route_table_id = aws_route_table.private[count.index].id
 }
 resource "aws_route_table_association" "private_e" {
-    subnet_id      = aws_subnet.private_subnet_5.id
-    route_table_id = aws_route_table.private.id
+  count          = 2
+  subnet_id      = aws_subnet.private_subnet_5.id
+  route_table_id = aws_route_table.private[count.index].id
 }
