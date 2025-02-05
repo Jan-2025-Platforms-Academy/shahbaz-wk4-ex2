1cb732175790efbbcf1edb602a9d0d43e2df8747
rearviewmirror <shahbaz.khan@kainos.com>
2025-02-05 00:30:19 +0000

    Adding codebuild and code pipeline

---
+++ pipeline/bb_codebuild_dev.tf
@@ -0,0 +1,25 @@
+resource "aws_codebuild_project" "dev" {
+    name = "shahbaz-dev-plan"
+    service_role = aws_iam_role.codebuild_role.arn
+    source {
+      type = "GITHUB"
+      location = "https://github.com/Jan-2025-Platforms-Academy/shahbaz-wk4-ex2"
+      buildspec = "buildspec.yml"
+    }
+    environment {
+        compute_type = "BUILD_GENERAL1_SMALL"
+        image = "aws/codebuild/standard:4.0"
+        type = "LINUX_CONTAINER"
+        environment_variable {
+            name = "TF_ACTION"
+            value = "plan"
+        }
+        environment_variable {
+            name = "ENVIRONMENT"
+            value = "dev"
+        }
+    }
+    artifacts {
+        type = "NO_ARTIFACTS"
+    }  
+}
